2fedfc5c8d84f1800913cef4d4ade4cfc8b03bbe
Martin Plaas <martin.plaas@gmail.com>
2017-11-17 22:08:32 +0100

    replaced google chart on homepage with chart.js

--- static/js/home_plot.js
+++ static/js/home_plot.js
@@ -1,68 +1,26 @@
-/*google.charts.load('current', {
-	packages : [ 'corechart', 'bar' ]
-});*/
+$(document).ready(function() {
+	getData();
 
-google.charts.load('current', {
-	'packages' : [ 'bar' ]
 });
-google.charts.setOnLoadCallback(getData);
 
-
-
-
-var options = {
-	water : {
-		height : 300,
-		legend : {
-			position : 'none'
-		},
-		chart : {
-			title : 'Wasserverbrauch der letzten 7 Tage'
-		},
-		colors : [ icon_mapping["water"]["color"], icon_mapping["water"]["color"] ]
-	},
-	gas: {
-		height : 300,
-		legend : {
-			position : 'none'
-		},
-		chart : {
-			title : 'Gasverbrauch der letzten 7 Tage'
-		},
-		colors : [ icon_mapping["gas"]["color"], icon_mapping["gas"]["color"] ]
-	},
-	power: {
-		height : 300,
-		legend : {
-			position : 'none'
-		},
-		chart : {
-			title : 'Stromverbrauch der letzten 7 Tage'
-		},
-		colors : [ icon_mapping["power"]["color"], icon_mapping["power"]["color"] ]
-	}
-};
-
-var data;
-var material;
 
 function getData() {
-	
-//	$.get({
-//		type : 'GET',
-//		url : '/sensor/_get_sensors',
-//		success : function (data) {sensors = data},
+
+	// $.get({
+	// type : 'GET',
+	// url : '/sensor/_get_sensors',
+	// success : function (data) {sensors = data},
+	// });
+
+//	$.get("/sensor/_get_sensors", function(data) {
+//		sensors = data;
 //	});
-	
-	$.get( "/sensor/_get_sensors", function( data ) {
-		sensors = data;
-		});
 
 	var today = Date.today();
 	var week_ago = Date.today().add(-6).days();
 	var from_date = week_ago.toString('dd.MM.yyyy');
 	var to_date = today.toString('dd.MM.yyyy');
-	
+
 	for (sensor in sensors) {
 		$.ajax({
 			type : 'POST',
@@ -78,47 +36,85 @@ function getData() {
 		});
 	}
 
-//	$.ajax({
-//		type : 'POST',
-//		url : '/charts/_get_chart?sensor=',
-//		data : {
-//			from_date : from_date,
-//			from_time : "00:00",
-//			to_date : to_date,
-//			to_time : "00:00",
-//			resolution : "86400"
-//		},
-//		success : drawChart,
-//	});
-//
-//	$.ajax({
-//		type : 'POST',
-//		url : '/water/_get_verbrauch',
-//		data : {
-//			von_date : from_date,
-//			von_uhr : "00:00",
-//			bis_date : to_date,
-//			bis_uhr : "00:00",
-//			aufloesung : "86400"
-//		},
-//		success : drawWaterChart,
-//	});
+	// $.ajax({
+	// type : 'POST',
+	// url : '/charts/_get_chart?sensor=',
+	// data : {
+	// from_date : from_date,
+	// from_time : "00:00",
+	// to_date : to_date,
+	// to_time : "00:00",
+	// resolution : "86400"
+	// },
+	// success : drawChart,
+	// });
+	//
+	// $.ajax({
+	// type : 'POST',
+	// url : '/water/_get_verbrauch',
+	// data : {
+	// von_date : from_date,
+	// von_uhr : "00:00",
+	// bis_date : to_date,
+	// bis_uhr : "00:00",
+	// aufloesung : "86400"
+	// },
+	// success : drawWaterChart,
+	// });
 
 }
 
 function drawChart(jsonData) {
-	
+
 	if (jsonData["status"] == "ok") {
 		sensor = jsonData["sensor"]
-		material = new google.charts.Bar(document.getElementById(sensor["id"]
-				+ '_chart_div'));
+		data = jsonData["data"];
+		labels = jsonData["labels"];
+		
+		var ctx = document.getElementById(sensor["id"] + '_chart_div').getContext('2d');
 
-		data = new google.visualization.DataTable(jsonData);
+		var chart = new Chart(
+				ctx,
+				{
+					// The type of chart we want to create
+					type : 'bar',
+
+					// The data for our dataset
+					data : {
+						labels : labels,
+						datasets : [ {
+							label : "My First dataset",
+							backgroundColor : icon_mapping[sensor.type]["color"],
+							borderColor : icon_mapping[sensor.type]["color"],
+							data : data,
+						} ]
+					},
+
+					// Configuration options go here
+					options : {
+						legend : {
+							display : false
+						},
+						scales : {
+							yAxes : [ {
+								ticks : {
+									beginAtZero : true,
+								}
+							} ]
+						},
+						tooltips : {
+							callbacks : {
+								afterFooter : function(tooltipItem, chart) {
+									return ("<br><br>" + tooltipItem[0].xLabel);
+								}
+							}
+						}
+					}
+				});
+
 
 		// Instantiate and draw our chart, passing in some options.
 
-		material.draw(data, options[sensor["type"]]);
 	}
-	
-	
+
 }
--- static/js/plot.js
+++ static/js/plot.js
@@ -1,13 +1,13 @@
 var sensor;
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	$.ajax({
 		type : 'POST',
 		url : '/charts/_get_chart?sensor=' + sensor,
 		data : $("#settings").serialize(),
 		success : drawChart,
 	});
-    
+
 });
 
 function setSensor(_sensor) {
@@ -19,43 +19,54 @@ var labels;
 var data;
 var ctx = document.getElementById('myChart').getContext('2d');
 
-
-
 function drawChart(jsonData) {
-	
+
 	data = jsonData["data"];
 	labels = jsonData["labels"];
 
 	chart = new Chart(ctx, {
-	    // The type of chart we want to create
-	    type: 'bar',
+		// The type of chart we want to create
+		type : 'bar',
 
-	    // The data for our dataset
-	    data: {
-	        labels: labels,
-	        datasets: [{
-	            label: "My First dataset",
-	            backgroundColor: icon_mapping[sensors[sensor].type]["color"],
-	            borderColor: icon_mapping[sensors[sensor].type]["color"],
-	            data: data,
-	        }]
-	    },
+		// The data for our dataset
+		data : {
+			labels : labels,
+			datasets : [ {
+				label : "My First dataset",
+				backgroundColor : icon_mapping[sensors[sensor].type]["color"],
+				borderColor : icon_mapping[sensors[sensor].type]["color"],
+				data : data,
+			} ]
+		},
 
-	    // Configuration options go here
-	    options: {
-	    	legend: {
-				display: false
+		// Configuration options go here
+		options : {
+			legend : {
+				display : false
 			},
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true,
+			scales : {
+				yAxes : [ {
+					ticks : {
+						beginAtZero : true,
 					}
-				}]
+				} ]
+			},
+			tooltips: {
+	            callbacks: {
+	            	afterFooter : function(tooltipItem, chart) {
+	            		return("<br><br>" + tooltipItem[0].xLabel);
+	            	}
+	            }
 			}
-	    }
+		}
 	});
-	
+	document.getElementById("myChart").onclick = function(evt) {
+		var element = chart.getElementAtEvent(evt)[0]
+		var clickedElementindex = element["_index"];
+
+		// get specific label by index
+		alert(chart.data.labels[clickedElementindex]);
+	}
 
 }
 
@@ -71,13 +82,12 @@ function requestNewData() {
 
 function newData(jsonData) {
 	if (jsonData["status"] == "ok") {
-		
+
 		chart.data.datasets[0].data = jsonData["data"];
 		chart.data.labels = jsonData["labels"];
 		chart.update();
 		// Instantiate and draw our chart, passing in some options.
 
-		
 		$("#loading_icon").fadeOut();
 	} else {
 		$("#loading_icon").fadeOut();
@@ -86,15 +96,11 @@ function newData(jsonData) {
 
 }
 
-// The select handler. Call the chart's getSelection() method
-function selectHandler() {
-  var selectedItem = material.getSelection()[0];
-  if (selectedItem) {
-    var value = data.getValue(selectedItem.row, 0);
-    alert('The user selected ' + value);
-  }
+function clickHandler(evt) {
+	var item = myChart.getElementAtEvent(evt)[0];
+
+	if (item) {
+		var label = myChart.data.labels[firstPoint._index];
+		var value = myChart.data.datasets[firstPoint._datasetIndex].data[firstPoint._index];
+	}
 }
-
-
-
-
